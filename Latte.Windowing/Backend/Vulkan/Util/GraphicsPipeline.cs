2016b2f1952612b8846e62100eb2d6087b51f91b
peter-r-g <petergorman11@gmail.com>
2023-06-17 15:46:59 -0400

    Bail from disposing GraphicsPipeline if it already is

--- Latte.Windowing/Backend/Vulkan/Util/GraphicsPipeline.cs
+++ Latte.Windowing/Backend/Vulkan/Util/GraphicsPipeline.cs
@@ -10,6 +10,8 @@ internal sealed class GraphicsPipeline : IDisposable
 	internal Pipeline Pipeline { get; }
 	internal PipelineLayout Layout { get; }
 
+	private bool disposed;
+
 	internal GraphicsPipeline( in Pipeline pipeline, in PipelineLayout layout, LogicalGpu owner )
 	{
 		Pipeline = pipeline;
@@ -17,10 +19,21 @@ internal sealed class GraphicsPipeline : IDisposable
 		Owner = owner;
 	}
 
+	~GraphicsPipeline()
+	{
+		Dispose();
+	}
+
 	public unsafe void Dispose()
 	{
+		if ( disposed )
+			return;
+
+		disposed = true;
 		Apis.Vk.DestroyPipeline( Owner, Pipeline, null );
 		Apis.Vk.DestroyPipelineLayout( Owner, Layout, null );
+
+		GC.SuppressFinalize( this );
 	}
 
 	public static implicit operator Pipeline( GraphicsPipeline graphicsPipeline ) => graphicsPipeline.Pipeline;
