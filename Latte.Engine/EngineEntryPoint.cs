1ee9321edd0f14d765231b778ad88e5a33dbfef7
peter-r-g <petergorman11@gmail.com>
2023-07-08 10:23:16 -0400

    Add Latte.Hotload and Latte.Engine projects

---
+++ Latte.Engine/EngineEntryPoint.cs
@@ -0,0 +1,20 @@
+﻿using Latte.Hotload;
+using System;
+
+namespace Latte.Engine;
+
+public sealed class EngineEntryPoint : IEntryPoint
+{
+	public void Main()
+	{
+		Console.WriteLine( "Hello from engine!" );
+	}
+
+	public void PreHotload()
+	{
+	}
+
+	public void PostHotload()
+	{
+	}
+}
