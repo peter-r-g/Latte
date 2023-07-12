6a4c8cf25a692bd65b37fcc3f333ca5d66a3317e
peter-r-g <petergorman11@gmail.com>
2023-07-12 17:25:31 -0400

    Add Latte.Assets, aggregate all assembly assets directory

---
+++ Latte.Assets/AssetsEntryPoint.cs
@@ -0,0 +1,61 @@
+﻿using Latte.Hotload;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using Zio;
+using Zio.FileSystems;
+
+namespace Latte.Assets;
+
+public sealed class AssetsEntryPoint : IEntryPoint
+{
+	private static ConcurrentDictionary<string, IFileSystem> AssemblyFileSystems { get; } = new();
+
+	public void Main()
+	{
+		foreach ( var (_, assembly) in HotloadableAssembly.All )
+			AddAssemblyAssets( assembly.AssemblyInfo );
+
+		HotloadableAssembly.OnAdded += OnAssemblyAdded;
+		HotloadableAssembly.OnRemoved += OnAssemblyRemoved;
+	}
+
+	public void PreHotload()
+	{
+		HotloadableAssembly.OnAdded -= OnAssemblyAdded;
+		HotloadableAssembly.OnRemoved -= OnAssemblyRemoved;
+	}
+
+	public void PostHotload()
+	{
+		HotloadableAssembly.OnAdded += OnAssemblyAdded;
+		HotloadableAssembly.OnRemoved += OnAssemblyRemoved;
+	}
+
+	private void OnAssemblyAdded( HotloadableAssembly hotloadableAssembly )
+	{
+		AddAssemblyAssets( hotloadableAssembly.AssemblyInfo );
+	}
+
+	private void OnAssemblyRemoved( HotloadableAssembly hotloadableAssembly )
+	{
+		if ( !AssemblyFileSystems.TryRemove( hotloadableAssembly.AssemblyInfo.Name, out var fs ) )
+			return;
+
+		FileSystems.InternalAssets.RemoveFileSystem( fs );
+		fs.Dispose();
+	}
+
+	private static void AddAssemblyAssets( in AssemblyInfo assemblyInfo )
+	{
+		if ( assemblyInfo.ProjectPath is null )
+			return;
+
+		var projectPath = FileSystems.System.ConvertPathFromInternal( assemblyInfo.ProjectPath );
+		if ( !FileSystems.System.DirectoryExists( projectPath / "Assets" ) )
+			return;
+
+		var fs = new SubFileSystem( FileSystems.System, projectPath / "Assets" );
+		AssemblyFileSystems.TryAdd( assemblyInfo.Name, fs );
+		FileSystems.InternalAssets.AddFileSystem( fs );
+	}
+}
---
+++ Latte.Assets/FileSystems.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Zio;
+using Zio.FileSystems;
+
+namespace Latte.Assets;
+
+public static class FileSystems
+{
+	public static IFileSystem System { get; private set; } = new PhysicalFileSystem();
+
+	public static IFileSystem Program { get; private set; } = new SubFileSystem(
+		System,
+		System.ConvertPathFromInternal( Directory.GetCurrentDirectory() ) );
+
+	public static IFileSystem Assets => InternalAssets;
+	internal static AggregateFileSystem InternalAssets { get; private set; } = new( Program );
+}
