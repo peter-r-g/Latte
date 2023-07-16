eec5001230289dd9721dc59fa010d02930dd0054
peter-r-g <petergorman11@gmail.com>
2023-07-15 23:12:44 -0400

    Re-organize LogicalGpu

--- Latte.Windowing/Backend/Vulkan/LogicalGpu.cs
+++ Latte.Windowing/Backend/Vulkan/LogicalGpu.cs
@@ -108,123 +108,6 @@ internal sealed class LogicalGpu : VulkanWrapper
 		Apis.Vk.FreeCommandBuffers( LogicalDevice, OneTimeCommandPool, 1, commandBuffer );
 	}
 
-	internal unsafe VulkanSwapchain CreateSwapchain()
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var swapchain = VulkanSwapchain.New( this );
-		DisposeQueue.Enqueue( swapchain.Dispose );
-		return swapchain;
-	}
-
-	internal unsafe VulkanGraphicsPipeline CreateGraphicsPipeline( IRenderingOptions options, Shader shader, in Extent2D swapchainExtent,
-		in VulkanRenderPass renderPass, in ReadOnlySpan<VertexInputBindingDescription> bindingDescriptions,
-		in ReadOnlySpan<VertexInputAttributeDescription> attributeDescriptions, in ReadOnlySpan<DynamicState> dynamicStates,
-		in ReadOnlySpan<VulkanDescriptorSetLayout> descriptorSetLayouts, in ReadOnlySpan<PushConstantRange> pushConstantRanges )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var graphicsPipeline = VulkanGraphicsPipeline.New( this, options, shader, swapchainExtent, renderPass, bindingDescriptions,
-			attributeDescriptions, dynamicStates, descriptorSetLayouts, pushConstantRanges );
-		DisposeQueue.Enqueue( graphicsPipeline.Dispose );
-		return graphicsPipeline;
-	}
-
-	internal unsafe VulkanDescriptorSetLayout CreateDescriptorSetLayout( in ReadOnlySpan<DescriptorSetLayoutBinding> bindings )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var descriptorSetLayout = VulkanDescriptorSetLayout.New( this, bindings );
-		DisposeQueue.Enqueue( descriptorSetLayout.Dispose );
-		return descriptorSetLayout;
-	}
-
-	internal unsafe VulkanRenderPass CreateRenderPass( Format swapchainImageFormat, SampleCountFlags msaaSamples )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var renderPass = VulkanRenderPass.New( this, swapchainImageFormat, msaaSamples );
-		DisposeQueue.Enqueue( renderPass.Dispose );
-		return renderPass;
-	}
-
-	internal unsafe VulkanCommandPool CreateCommandPool( uint queueFamilyIndex )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var commandPool = VulkanCommandPool.New( this, queueFamilyIndex );
-		DisposeQueue.Enqueue( commandPool.Dispose );
-		return commandPool;
-	}
-
-	internal unsafe VulkanDescriptorPool CreateDescriptorPool( in ReadOnlySpan<DescriptorPoolSize> descriptorPoolSizes, uint maxDescriptorSets )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var descriptorPool = VulkanDescriptorPool.New( this, descriptorPoolSizes, maxDescriptorSets );
-		DisposeQueue.Enqueue( descriptorPool.Dispose );
-		return descriptorPool;
-	}
-
-	internal unsafe VulkanBuffer CreateBuffer( ulong size, BufferUsageFlags usageFlags, MemoryPropertyFlags memoryFlags,
-		SharingMode sharingMode = SharingMode.Exclusive )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var buffer = VulkanBuffer.New( this, size, usageFlags, memoryFlags, sharingMode );
-		DisposeQueue.Enqueue( buffer.Dispose );
-		return buffer;
-	}
-
-	internal unsafe VulkanImage CreateImage( uint width, uint height, uint mipLevels, SampleCountFlags numSamples,
-		Format format, ImageTiling tiling, ImageUsageFlags usageFlags, MemoryPropertyFlags memoryPropertyFlags, ImageAspectFlags aspectFlags )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var image = VulkanImage.New( this, width, height, mipLevels, numSamples, format,
-			tiling, usageFlags, memoryPropertyFlags, aspectFlags );
-		DisposeQueue.Enqueue( image.Dispose );
-		return image;
-	}
-
-	internal unsafe VulkanSampler CreateTextureSampler( bool enableMsaa, uint mipLevels )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var sampler = VulkanSampler.New( this, enableMsaa, mipLevels );
-		DisposeQueue.Enqueue( sampler.Dispose );
-		return sampler;
-	}
-
-	internal unsafe VulkanSemaphore CreateSemaphore()
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var semaphore = VulkanSemaphore.New( this );
-		DisposeQueue.Enqueue( semaphore.Dispose );
-		return semaphore;
-	}
-
-	internal unsafe VulkanFence CreateFence( bool signaled = false )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var fence = VulkanFence.New( this, signaled );
-		DisposeQueue.Enqueue( fence.Dispose );
-		return fence;
-	}
-
 	internal unsafe void GetMeshGpuBuffers( VulkanBackend vulkanBackend, Mesh mesh, out GpuBuffer<Vertex> gpuVertexBuffer, out GpuBuffer<uint>? gpuIndexBuffer )
 	{
 		if ( Disposed )
@@ -299,7 +182,7 @@ internal sealed class LogicalGpu : VulkanWrapper
 			{
 				ImageLayout = ImageLayout.ShaderReadOnlyOptimal,
 				ImageView = textureImage.View,
-				Sampler = CreateTextureSampler( numSamples != SampleCountFlags.Count1Bit, texture.MipLevels )
+				Sampler = CreateSampler( numSamples != SampleCountFlags.Count1Bit, texture.MipLevels )
 			};
 
 			var uboWrite = new WriteDescriptorSet
@@ -333,6 +216,133 @@ internal sealed class LogicalGpu : VulkanWrapper
 		return descriptorSets;
 	}
 
+	internal unsafe VulkanBuffer CreateBuffer( ulong size, BufferUsageFlags usageFlags, MemoryPropertyFlags memoryFlags,
+		SharingMode sharingMode = SharingMode.Exclusive )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var buffer = VulkanBuffer.New( this, size, usageFlags, memoryFlags, sharingMode );
+		DisposeQueue.Enqueue( buffer.Dispose );
+		return buffer;
+	}
+
+	internal unsafe VulkanCommandPool CreateCommandPool( uint queueFamilyIndex )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var commandPool = VulkanCommandPool.New( this, queueFamilyIndex );
+		DisposeQueue.Enqueue( commandPool.Dispose );
+		return commandPool;
+	}
+
+	internal unsafe VulkanDescriptorPool CreateDescriptorPool( in ReadOnlySpan<DescriptorPoolSize> descriptorPoolSizes, uint maxDescriptorSets )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var descriptorPool = VulkanDescriptorPool.New( this, descriptorPoolSizes, maxDescriptorSets );
+		DisposeQueue.Enqueue( descriptorPool.Dispose );
+		return descriptorPool;
+	}
+
+	internal unsafe VulkanDescriptorSetLayout CreateDescriptorSetLayout( in ReadOnlySpan<DescriptorSetLayoutBinding> bindings )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var descriptorSetLayout = VulkanDescriptorSetLayout.New( this, bindings );
+		DisposeQueue.Enqueue( descriptorSetLayout.Dispose );
+		return descriptorSetLayout;
+	}
+
+	internal unsafe VulkanFence CreateFence( bool signaled = false )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var fence = VulkanFence.New( this, signaled );
+		DisposeQueue.Enqueue( fence.Dispose );
+		return fence;
+	}
+
+	internal unsafe VulkanGraphicsPipeline CreateGraphicsPipeline( IRenderingOptions options, Shader shader, in Extent2D swapchainExtent,
+		in VulkanRenderPass renderPass, in ReadOnlySpan<VertexInputBindingDescription> bindingDescriptions,
+		in ReadOnlySpan<VertexInputAttributeDescription> attributeDescriptions, in ReadOnlySpan<DynamicState> dynamicStates,
+		in ReadOnlySpan<VulkanDescriptorSetLayout> descriptorSetLayouts, in ReadOnlySpan<PushConstantRange> pushConstantRanges )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var graphicsPipeline = VulkanGraphicsPipeline.New( this, options, shader, swapchainExtent, renderPass, bindingDescriptions,
+			attributeDescriptions, dynamicStates, descriptorSetLayouts, pushConstantRanges );
+		DisposeQueue.Enqueue( graphicsPipeline.Dispose );
+		return graphicsPipeline;
+	}
+
+	internal unsafe VulkanImage CreateImage( uint width, uint height, uint mipLevels, SampleCountFlags numSamples,
+		Format format, ImageTiling tiling, ImageUsageFlags usageFlags, MemoryPropertyFlags memoryPropertyFlags, ImageAspectFlags aspectFlags )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var image = VulkanImage.New( this, width, height, mipLevels, numSamples, format,
+			tiling, usageFlags, memoryPropertyFlags, aspectFlags );
+		DisposeQueue.Enqueue( image.Dispose );
+		return image;
+	}
+
+	internal unsafe VulkanRenderPass CreateRenderPass( Format swapchainImageFormat, SampleCountFlags msaaSamples )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var renderPass = VulkanRenderPass.New( this, swapchainImageFormat, msaaSamples );
+		DisposeQueue.Enqueue( renderPass.Dispose );
+		return renderPass;
+	}
+
+	internal unsafe VulkanSampler CreateSampler( bool enableMsaa, uint mipLevels )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var sampler = VulkanSampler.New( this, enableMsaa, mipLevels );
+		DisposeQueue.Enqueue( sampler.Dispose );
+		return sampler;
+	}
+
+	internal unsafe VulkanSemaphore CreateSemaphore()
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var semaphore = VulkanSemaphore.New( this );
+		DisposeQueue.Enqueue( semaphore.Dispose );
+		return semaphore;
+	}
+
+	internal unsafe VulkanShaderModule CreateShaderModule( in ReadOnlySpan<byte> shaderCode )
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var shaderModule = VulkanShaderModule.New( this, shaderCode );
+		DisposeQueue.Enqueue( shaderModule.Dispose );
+		return shaderModule;
+	}
+
+	internal unsafe VulkanSwapchain CreateSwapchain()
+	{
+		if ( Disposed )
+			throw new ObjectDisposedException( nameof( LogicalGpu ) );
+
+		var swapchain = VulkanSwapchain.New( this );
+		DisposeQueue.Enqueue( swapchain.Dispose );
+		return swapchain;
+	}
+
 	internal uint FindMemoryType( uint typeFilter, MemoryPropertyFlags properties )
 	{
 		if ( Disposed )
@@ -346,16 +356,6 @@ internal sealed class LogicalGpu : VulkanWrapper
 		}
 
 		throw new ApplicationException( "Failed to find suitable memory type" );
-	}
-
-	internal unsafe VulkanShaderModule CreateShaderModule( in ReadOnlySpan<byte> shaderCode )
-	{
-		if ( Disposed )
-			throw new ObjectDisposedException( nameof( LogicalGpu ) );
-
-		var shaderModule = VulkanShaderModule.New( this, shaderCode );
-		DisposeQueue.Enqueue( shaderModule.Dispose );
-		return shaderModule;
 	}
 
 	internal unsafe ImageView CreateImageView( in Image image, Format format, ImageAspectFlags aspectFlags, uint mipLevels )
