a1dcede1f8a90612edee719a7d0d7a0958966178
peter-r-g <petergorman11@gmail.com>
2023-07-15 21:03:31 -0400

    Remove old unused shader compile files

--- Sandbox/compile_shaders_glsl.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-C:\VulkanSDK\1.3.224.1\Bin\glslc.exe -fshader-stage=vert Assets\Shaders\vert.glsl -o Assets\Shaders\vert.spv
-C:\VulkanSDK\1.3.224.1\Bin\glslc.exe -fshader-stage=frag Assets\Shaders\frag.glsl -o Assets\Shaders\frag.spv
-
-PAUSE
