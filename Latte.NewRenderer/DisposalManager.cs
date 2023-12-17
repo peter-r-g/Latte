b22e25b7da9b07f7ca0027f1736a498458bf5a2d
Peter <tamewater.gunman@gmail.com>
2023-12-17 00:30:51 -0500

    Remove associated references to disposal callbacks

--- Latte.NewRenderer/DisposalManager.cs
+++ Latte.NewRenderer/DisposalManager.cs
@@ -7,6 +7,7 @@ internal sealed class DisposalManager : IDisposable
 {
 	private readonly List<Action> disposals = [];
 	private readonly Dictionary<string, List<WeakReference<Action>>> taggedDisposals = [];
+	private readonly Dictionary<WeakReference<Action>, string[]> disposalTags = [];
 
 	private bool disposed;
 
@@ -18,12 +19,17 @@ internal sealed class DisposalManager : IDisposable
 	internal void Add( Action cb, params string[] tags )
 	{
 		disposals.Add( cb );
+
+		var weakCb = new WeakReference<Action>( cb );
+		disposalTags.Add( weakCb, tags );
+
 		foreach ( var tag in tags )
 		{
 			if ( !taggedDisposals.ContainsKey( tag ) )
 				taggedDisposals.Add( tag, [] );
 
-			taggedDisposals[tag].Add( new WeakReference<Action>( cb ) );
+			
+			taggedDisposals[tag].Add( weakCb );
 		}
 	}
 
@@ -34,14 +40,33 @@ internal sealed class DisposalManager : IDisposable
 
 		for ( var i = disposals.Count - 1; i >= 0; i-- )
 		{
-			if ( !disposals[i].TryGetTarget( out var cb ) )
-				continue;
+			var weakCb = disposals[i];
 
-			this.disposals.Remove( cb );
+			if ( !weakCb.TryGetTarget( out var cb ) )
+			{
+				RemoveAssociatedReferences( weakCb );
+				continue;
+			}
+
+			RemoveAssociatedReferences( weakCb );
 			cb();
 		}
 
 		disposals.Clear();
+	}
+
+	private void RemoveAssociatedReferences( WeakReference<Action> weakCb )
+	{
+		if ( !disposalTags.TryGetValue( weakCb, out var tags ) )
+			return;
+
+		foreach ( var tag in tags )
+			taggedDisposals[tag].Remove( weakCb );
+
+		disposalTags.Remove( weakCb );
+
+		if ( weakCb.TryGetTarget( out var cb ) )
+			disposals.Remove( cb );
 	}
 
 	private void Dispose( bool disposing )
