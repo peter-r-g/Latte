57fb6b63347b5122445261a186c717d619c5f44e
peter-r-g <petergorman11@gmail.com>
2023-12-19 08:16:41 -0500

    Spawn window from another thread

--- Latte.NewRenderer/Program.cs
+++ Latte.NewRenderer/Program.cs
@@ -4,6 +4,7 @@ using Silk.NET.Windowing;
 using Silk.NET.Windowing.Glfw;
 using System;
 using System.Numerics;
+using System.Threading;
 using Window = Silk.NET.Windowing.Window;
 
 namespace Latte.NewRenderer;
@@ -15,7 +16,26 @@ internal static class Program
 	private static InputManager input = null!;
 	private static Vector2 lastMousePosition;
 
+	private static bool isWindowActive;
+
 	private static unsafe void Main()
+	{
+		var windowThread = new Thread( WindowMain );
+		windowThread.Start();
+		isWindowActive = true;
+
+		string line;
+		do
+		{
+			line = ReadLine();
+			HandleCommand( line );
+		} while ( line != "quit" && isWindowActive );
+
+		window.Close();
+		windowThread.Join();
+	}
+
+	private static unsafe void WindowMain()
 	{
 		GlfwWindowing.Use();
 		var options = WindowOptions.DefaultVulkan with
@@ -53,6 +73,7 @@ internal static class Program
 		engine.Dispose();
 		input.Cleanup();
 		window.Dispose();
+		isWindowActive = false;
 	}
 
 	private static unsafe void Update( double dt )
@@ -117,5 +138,37 @@ internal static class Program
 			MathF.Sin( Scalar.DegreesToRadians( Camera.Main.Pitch ) ),
 			MathF.Sin( Scalar.DegreesToRadians( Camera.Main.Yaw ) ) * MathF.Cos( Scalar.DegreesToRadians( Camera.Main.Pitch ) )
 		);
+	}
+
+	private static string ReadLine()
+	{
+		var line = string.Empty;
+		var key = default( ConsoleKeyInfo );
+		do
+		{
+			if ( !Console.KeyAvailable )
+				continue;
+
+			key = Console.ReadKey( true );
+			if ( char.IsAsciiLetterOrDigit( key.KeyChar ) || key.Key == ConsoleKey.Spacebar )
+			{
+				line += key.KeyChar;
+				Console.Write( key.KeyChar );
+			}
+
+			if ( key.Key == ConsoleKey.Backspace )
+			{
+				line = line[..^1];
+				Console.WriteLine();
+				Console.Write( line );
+			}
+		} while ( key.Key != ConsoleKey.Enter && isWindowActive );
+		Console.WriteLine();
+
+		return line;
+	}
+
+	private static void HandleCommand( string cmd )
+	{
 	}
 }
