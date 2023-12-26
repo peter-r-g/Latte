49a6ac1b08a85f80e5815f5fe2322cdfb6bcfbaf
peter-r-g <petergorman11@gmail.com>
2023-12-26 18:42:28 -0500

    Add F1-24 inputs and tweak extensions

--- Latte.NewRenderer/Extensions/InputButtonExtensions.cs
+++ Latte.NewRenderer/Extensions/InputButtonExtensions.cs
@@ -16,7 +16,7 @@ public static class InputButtonExtensions
 	/// <returns>whether or not the button is a keyboard input.</returns>
 	public static bool IsKeyboardInput( this InputButton button )
 	{
-		return button >= InputButton.KeyboardW && button <= InputButton.KeyboardLeftShift;
+		return button > InputButton.KEYBOARD && button < InputButton.COUNT;
 	}
 
 	/// <summary>
@@ -26,7 +26,7 @@ public static class InputButtonExtensions
 	/// <returns>Whether or not the button is a mouse button.</returns>
 	public static bool IsMouseInput( this InputButton button )
 	{
-		return button >= InputButton.LMB && button <= InputButton.RMB;
+		return button > InputButton.MOUSE && button < InputButton.KEYBOARD;
 	}
 
 	/// <summary>
@@ -48,6 +48,30 @@ public static class InputButtonExtensions
 			InputButton.KeyboardTab => Key.Tab,
 			InputButton.KeyboardEscape => Key.Escape,
 			InputButton.KeyboardLeftShift => Key.ShiftLeft,
+			InputButton.KeyboardF1 => Key.F1,
+			InputButton.KeyboardF2 => Key.F2,
+			InputButton.KeyboardF3 => Key.F3,
+			InputButton.KeyboardF4 => Key.F4,
+			InputButton.KeyboardF5 => Key.F5,
+			InputButton.KeyboardF6 => Key.F6,
+			InputButton.KeyboardF7 => Key.F7,
+			InputButton.KeyboardF8 => Key.F8,
+			InputButton.KeyboardF9 => Key.F9,
+			InputButton.KeyboardF10 => Key.F10,
+			InputButton.KeyboardF11 => Key.F11,
+			InputButton.KeyboardF12 => Key.F12,
+			InputButton.KeyboardF13 => Key.F13,
+			InputButton.KeyboardF14 => Key.F14,
+			InputButton.KeyboardF15 => Key.F15,
+			InputButton.KeyboardF16 => Key.F16,
+			InputButton.KeyboardF17 => Key.F17,
+			InputButton.KeyboardF18 => Key.F18,
+			InputButton.KeyboardF19 => Key.F19,
+			InputButton.KeyboardF20 => Key.F20,
+			InputButton.KeyboardF21 => Key.F21,
+			InputButton.KeyboardF22 => Key.F22,
+			InputButton.KeyboardF23 => Key.F23,
+			InputButton.KeyboardF24 => Key.F24,
 			_ => throw new ArgumentException( $"Received non keyboard key \"{button}\"", nameof( button ) )
 		};
 	}
--- Latte.NewRenderer/Input/InputButton.cs
+++ Latte.NewRenderer/Input/InputButton.cs
@@ -5,9 +5,11 @@
 /// </summary>
 public enum InputButton
 {
+	MOUSE,
 	LMB,
 	RMB,
 	MMB,
+	KEYBOARD,
 	KeyboardW,
 	KeyboardA,
 	KeyboardS,
@@ -16,5 +18,30 @@ public enum InputButton
 	KeyboardE,
 	KeyboardTab,
 	KeyboardEscape,
-	KeyboardLeftShift
+	KeyboardLeftShift,
+	KeyboardF1,
+	KeyboardF2,
+	KeyboardF3,
+	KeyboardF4,
+	KeyboardF5,
+	KeyboardF6,
+	KeyboardF7,
+	KeyboardF8,
+	KeyboardF9,
+	KeyboardF10,
+	KeyboardF11,
+	KeyboardF12,
+	KeyboardF13,
+	KeyboardF14,
+	KeyboardF15,
+	KeyboardF16,
+	KeyboardF17,
+	KeyboardF18,
+	KeyboardF19,
+	KeyboardF20,
+	KeyboardF21,
+	KeyboardF22,
+	KeyboardF23,
+	KeyboardF24,
+	COUNT
 }
