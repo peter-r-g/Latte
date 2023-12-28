c808630ab2cd58e76f19f26d159b8d138c496171
peter-r-g <petergorman11@gmail.com>
2023-12-27 23:49:48 -0500

    Remove unused shaders

--- Latte.NewRenderer/Shaders/compile_debug.bat
+++ Latte.NewRenderer/Shaders/compile_debug.bat
@@ -1,10 +1,6 @@
 @ECHO OFF
 mkdir ..\bin\Debug\net8.0\Assets\Shaders
 
-%VULKAN_SDK%\Bin\glslc.exe colored_triangle.vert -o ..\bin\Debug\net8.0\Assets\Shaders\colored_triangle.vert.spv
-%VULKAN_SDK%\Bin\glslc.exe colored_triangle.frag -o ..\bin\Debug\net8.0\Assets\Shaders\colored_triangle.frag.spv
-%VULKAN_SDK%\Bin\glslc.exe triangle.vert -o ..\bin\Debug\net8.0\Assets\Shaders\triangle.vert.spv
-%VULKAN_SDK%\Bin\glslc.exe triangle.frag -o ..\bin\Debug\net8.0\Assets\Shaders\triangle.frag.spv
 %VULKAN_SDK%\Bin\glslc.exe mesh_triangle.vert -o ..\bin\Debug\net8.0\Assets\Shaders\mesh_triangle.vert.spv
 %VULKAN_SDK%\Bin\glslc.exe default_lit.frag -o ..\bin\Debug\net8.0\Assets\Shaders\default_lit.frag.spv
 %VULKAN_SDK%\Bin\glslc.exe textured_lit.frag -o ..\bin\Debug\net8.0\Assets\Shaders\textured_lit.frag.spv
--- Latte.NewRenderer/Shaders/compile_release.bat
+++ Latte.NewRenderer/Shaders/compile_release.bat
@@ -1,10 +1,6 @@
 @ECHO OFF
 mkdir ..\bin\Release\net8.0\Assets\Shaders
 
-%VULKAN_SDK%\Bin\glslc.exe colored_triangle.vert -o ..\bin\Release\net8.0\Assets\Shaders\colored_triangle.vert.spv
-%VULKAN_SDK%\Bin\glslc.exe colored_triangle.frag -o ..\bin\Release\net8.0\Assets\Shaders\colored_triangle.frag.spv
-%VULKAN_SDK%\Bin\glslc.exe triangle.vert -o ..\bin\Release\net8.0\Assets\Shaders\triangle.vert.spv
-%VULKAN_SDK%\Bin\glslc.exe triangle.frag -o ..\bin\Release\net8.0\Assets\Shaders\triangle.frag.spv
 %VULKAN_SDK%\Bin\glslc.exe mesh_triangle.vert -o ..\bin\Release\net8.0\Assets\Shaders\mesh_triangle.vert.spv
 %VULKAN_SDK%\Bin\glslc.exe default_lit.frag -o ..\bin\Release\net8.0\Assets\Shaders\default_lit.frag.spv
 %VULKAN_SDK%\Bin\glslc.exe textured_lit.frag -o ..\bin\Release\net8.0\Assets\Shaders\textured_lit.frag.spv
