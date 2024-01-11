3be2f4a8cae9fbe1ec45c5f7296dfb5493a13de2
peter-r-g <petergorman11@gmail.com>
2024-01-11 01:02:29 -0500

    Fix Allocation memory type name

--- Latte.NewRenderer/Vulkan/Allocations/Allocation.cs
+++ Latte.NewRenderer/Vulkan/Allocations/Allocation.cs
@@ -4,10 +4,10 @@ using System.Diagnostics.CodeAnalysis;
 namespace Latte.NewRenderer.Vulkan.Allocations;
 
 [method: SetsRequiredMembers]
-internal struct Allocation( DeviceMemory memory, uint memoryTypeBits, ulong offset, ulong size )
+internal struct Allocation( DeviceMemory memory, uint memoryType, ulong offset, ulong size )
 {
 	internal required DeviceMemory Memory = memory;
-	internal required uint MemoryType = memoryTypeBits;
+	internal required uint MemoryType = memoryType;
 	internal required ulong Offset = offset;
 	internal required ulong Size = size;
 }
