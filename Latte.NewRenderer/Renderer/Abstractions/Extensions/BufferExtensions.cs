7d3c203143341680460f53986492526cbc82260c
peter-r-g <petergorman11@gmail.com>
2024-01-24 21:03:37 -0500

    Allow stack allocate on buffer extensions

--- Latte.NewRenderer/Renderer/Abstractions/Extensions/BufferExtensions.cs
+++ Latte.NewRenderer/Renderer/Abstractions/Extensions/BufferExtensions.cs
@@ -7,26 +7,51 @@ namespace Latte.NewRenderer.Renderer.Abstractions.Extensions;
 
 internal static class BufferExtensions
 {
+	private const int MaxStackAllocBytes = 1024;
+
 	internal static unsafe void SetData<T>( this Buffer buffer, T data ) where T : unmanaged
 	{
 		var size = Unsafe.SizeOf<T>();
-		var ptr = Marshal.AllocHGlobal( size );
-		Unsafe.Copy( (void*)ptr, in data );
 
-		buffer.SetData( (void*)ptr, (ulong)size );
+		if ( size <= MaxStackAllocBytes )
+		{
+			void* ptr = stackalloc byte[size];
+			Unsafe.Copy( ptr, in data );
 
-		Marshal.FreeHGlobal( ptr );
+			buffer.SetData( ptr, (ulong)size );
+		}
+		else
+		{
+			var ptr = Marshal.AllocHGlobal( size );
+			Unsafe.Copy( (void*)ptr, in data );
+
+			buffer.SetData( (void*)ptr, (ulong)size );
+
+			Marshal.FreeHGlobal( ptr );
+		}
 	}
 
 	internal static unsafe void SetData<T>( this Buffer buffer, ReadOnlySpan<T> data ) where T : unmanaged
 	{
 		var size = Unsafe.SizeOf<T>();
-		var ptr = Marshal.AllocHGlobal( size );
-		for ( var i = 0; i < data.Length; i++ )
-			Unsafe.Copy( (void*)(ptr + size * i), in data[i] );
+		
+		if ( size <= MaxStackAllocBytes )
+		{
+			byte* ptr = stackalloc byte[size];
+			for ( var i = 0; i < data.Length; i++ )
+				Unsafe.Copy( (ptr + size * i), in data[i] );
 
-		buffer.SetData( (void*)ptr, (ulong)size );
+			buffer.SetData( ptr, (ulong)size );
+		}
+		else
+		{
+			var ptr = Marshal.AllocHGlobal( size );
+			for ( var i = 0; i < data.Length; i++ )
+				Unsafe.Copy( (void*)(ptr + size * i), in data[i] );
 
-		Marshal.FreeHGlobal( ptr );
+			buffer.SetData( (void*)ptr, (ulong)size );
+
+			Marshal.FreeHGlobal( ptr );
+		}
 	}
 }
