ceb6fc40f6de5da91ad718a93ced9c11670c23e1
peter-r-g <petergorman11@gmail.com>
2024-01-24 20:31:03 -0500

    Add vertex array abstraction

---
+++ Latte.NewRenderer/Renderer/VertexArray.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Latte.NewRenderer.Renderer;
+
+internal abstract class VertexArray
+{
+	internal abstract IEnumerable<VertexBuffer> VertexBuffers { get; }
+	internal abstract IndexBuffer IndexBuffer { get; set; }
+
+	internal abstract void Bind();
+	internal abstract void Unbind();
+
+	internal abstract void AddVertexBuffer( VertexBuffer vbo );
+
+	internal static VertexArray Create() => RendererApi.Current.Api switch
+	{
+		RenderApi.None => throw new NotImplementedException(),
+		RenderApi.Vulkan => throw new NotImplementedException(),
+		_ => throw new InvalidOperationException( "" )
+	};
+}
